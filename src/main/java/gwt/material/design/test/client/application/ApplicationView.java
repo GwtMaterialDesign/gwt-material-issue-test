267557365772e50911a4d59c8257adaa5e64088e
Kevzlou7979 <kevzlou7979@gmail.com>
2017-03-27 16:12:03 +0800

    Added demo test - for issue addins #197 MaterialComboBox issue with multiple selection

--- src/main/java/gwt/material/design/test/client/application/ApplicationView.java
+++ src/main/java/gwt/material/design/test/client/application/ApplicationView.java
@@ -25,39 +25,50 @@ import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.uibinder.client.UiHandler;
 import com.google.gwt.user.client.Timer;
-import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Widget;
 import com.gwtplatform.mvp.client.ViewImpl;
-import gwt.material.design.addins.client.window.MaterialWindow;
-import gwt.material.design.client.constants.Color;
-import gwt.material.design.client.ui.MaterialDatePicker;
-import gwt.material.design.client.ui.MaterialLoader;
+import gwt.material.design.addins.client.combobox.MaterialComboBox;
+import gwt.material.design.client.ui.MaterialButton;
 import gwt.material.design.client.ui.MaterialToast;
-import gwt.material.design.test.client.application.service.Car;
-import gwt.material.design.test.client.application.service.CarServiceAsync;
-import gwt.material.design.test.client.application.service.FakeCarService;
+import gwt.material.design.test.client.application.dto.SampleModel;
 
 import javax.inject.Inject;
-import java.util.Date;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Arrays;
 
 public class ApplicationView extends ViewImpl implements ApplicationPresenter.MyView {
     interface Binder extends UiBinder<Widget, ApplicationView> {
     }
 
     @UiField
-    MaterialWindow window;
+    MaterialComboBox<SampleModel> comboBox;
+
+    @UiField
+    MaterialButton btnGetSelectedValues, btnSetValues;
 
     @Inject
     ApplicationView(
             Binder uiBinder) {
         initWidget(uiBinder.createAndBindUi(this));
 
-        Timer t = new Timer() {
-            @Override
-            public void run() {
-                window.open();
+        comboBox.setKeyFactory(SampleModel::getId);
+
+        List<SampleModel> models = new ArrayList<>();
+        models.add(new SampleModel("1", "name1"));
+        models.add(new SampleModel("2", "name2"));
+        models.add(new SampleModel("3", "name3"));
+        comboBox.setAcceptableValues(models);
+
+
+        btnSetValues.addClickHandler(clickEvent -> {
+            comboBox.setValues(models);
+        });
+
+        btnGetSelectedValues.addClickHandler(clickEvent -> {
+            for (SampleModel model : comboBox.getSelectedValues()) {
+                MaterialToast.fireToast(model.getName());
             }
-        };
-        t.schedule(1000);
+        });
     }
 }

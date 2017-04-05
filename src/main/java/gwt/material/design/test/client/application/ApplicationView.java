49866e74376f52323c91f2b19ff608726d715a86
Kevzlou7979 <kevzlou7979@gmail.com>
2017-04-05 23:32:26 +0800

    Initial delivery and tests on  table style.

--- src/main/java/gwt/material/design/test/client/application/ApplicationView.java
+++ src/main/java/gwt/material/design/test/client/application/ApplicationView.java
@@ -19,36 +19,15 @@
  */
 package gwt.material.design.test.client.application;
 
-import com.google.gwt.core.client.GWT;
-import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.uibinder.client.UiBinder;
-import com.google.gwt.uibinder.client.UiField;
-import com.google.gwt.uibinder.client.UiHandler;
-import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Widget;
 import com.gwtplatform.mvp.client.ViewImpl;
-import gwt.material.design.addins.client.combobox.MaterialComboBox;
-import gwt.material.design.client.constants.SideNavType;
-import gwt.material.design.client.ui.*;
-import gwt.material.design.test.client.application.service.Car;
-import gwt.material.design.test.client.application.service.CarServiceAsync;
-import gwt.material.design.test.client.application.service.FakeCarService;
 
 import javax.inject.Inject;
-import java.util.Date;
 
 public class ApplicationView extends ViewImpl implements ApplicationPresenter.MyView {
     interface Binder extends UiBinder<Widget, ApplicationView> {
     }
-
-    @UiField
-    MaterialComboBox<String> combo;
-
-    @UiField
-    MaterialSideNav sideNav;
-
-    @UiField
-    MaterialHeader header;
 
     @Inject
     ApplicationView(
